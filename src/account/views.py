ed02c4a246fffe5c335963e8707e212956976f4b
gringows <esteban.ams95@gmail.com>
2020-07-18 17:20:21 -0400

    added the htmls of the account, login doesn't work

--- src/account/views.py
+++ src/account/views.py
@@ -10,11 +10,7 @@ def registration_view(request):
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
 			form.save()
-			email = form.cleaned_data.get('email')
-			raw_password = form.cleaned_data.get('password1')
-			account = authenticate(email=email, password=raw_password)
-			login(request, account)
-			return redirect('home')
+			return redirect('index')
 		else:
 			context['registration_form'] = form
 
