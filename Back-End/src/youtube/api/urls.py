101eb10fb5b124f10895edfa05140b89238a4e1b
gringows <esteban.ams95@gmail.com>
2020-08-22 03:04:43 -0400

    class-based generic views for APIs

--- Back-End/src/youtube/api/urls.py
+++ Back-End/src/youtube/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from youtube.api import views
 
 urlpatterns = [
-    path('video_detail/<int:pk>/', views.video_detail),
-    path('video_list/', views.video_list),
+    path('video_detail/<int:pk>/', views.VideoDetail.as_view()),
+    path('video_list/', views.VideoList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- Back-End/src/youtube/api/views.py
+++ Back-End/src/youtube/api/views.py
@@ -1,47 +1,65 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from youtube.models import Video, Comment
 from youtube.api.serlializers import VideoSerlializer, CommentSerializer
+from rest_framework import generics
 
-@csrf_exempt
-def video_list(request):
-    
-    if request.method == 'GET':
+class VideoList(generics.ListCreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerlializer
+
+class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerlializer
+
+
+
+
+
+
+
+"""
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+class VideoList(APIView):
+
+    def get(self, request, format=None):    
         video = Video.objects.all()
         serializer = VideoSerlializer(video, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = VideoSerlializer(data=data)
+    def post(self, request, format=None):
+        serializer = VideoSerlializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.error, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-
-def video_detail(request, pk):
+class VideoDetail(APIView):
 
     error_message = 'Oops!!  The object does not exist, try creating a new one!'
 
-    try:
-        video = Video.objects.get(pk=pk)
-    except Video.DoesNotExist:
-        return HttpResponse(error_message, status=404)
+    def get_object(self, pk):
+        try:
+            video = Video.objects.get(pk=pk)
+        except Video.DoesNotExist:
+            return Response(error_message, status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        video = self.get_object(pk)
         serializer = VideoSerlializer(video)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser.parse(request)
-        serializer = VideoSerlializer(video, data=data)
+    def put(self, request, pk, format=None):
+        video = self.get_object(pk)
+        serializer = VideoSerlializer(video, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.error, status=400)
+            return Response(serializer.data)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        video = self.get_object(pk)
         video.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_200_OK)
+"""
